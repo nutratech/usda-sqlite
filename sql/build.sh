1f0eee046092464faff74656469917f6dd935402
Shane Jaroch <nutratracker@protonmail.com>
2020-09-20 16:25:38 -0400

    rename to usda

--- sql/build.sh
+++ sql/build.sh
@@ -10,20 +10,20 @@ fi
 cd "$(dirname "$0")"
 # printf "\\n\e[1;31m\e[0m\\n"
 
-rm_cmd="rm -f nutra.db"
+rm_cmd="rm -f usda.db"
 printf "\\n\e[1;31m${rm_cmd}\e[0m\\n\n"
 $rm_cmd
 
-pack_msg="==> Pack nutra.db-$VERSION"
+pack_msg="==> Pack usda.db-$VERSION"
 printf "\\n\\x1b[32m${pack_msg}\x1b[0m\n\n"
 
 # Create SQL file
-pack_cmd="sqlite3 nutra.db \".read init.sql\""
+pack_cmd="sqlite3 usda.db \".read init.sql\""
 printf "\\n\e[1;31m${pack_cmd}\e[0m\\n"
 bash -exec "$pack_cmd"
 
 # Compress xzip
-tar_cmd="tar cJvf nutra.db-$VERSION.tar.xz nutra.db"
+tar_cmd="tar cJvf usda.db-$VERSION.tar.xz usda.db"
 printf "\\n\e[1;31m${tar_cmd}\e[0m\\n"
 $tar_cmd
 
@@ -31,6 +31,6 @@ $tar_cmd
 # printf "\\n\e[1;31m${rm_cmd}\e[0m\\n"
 # $rm_cmd
 
-mv_cmd="mv nutra.db-$VERSION.tar.xz dist"
+mv_cmd="mv usda.db-$VERSION.tar.xz dist"
 printf "\\n\e[1;31m${mv_cmd}\e[0m\\n\n"
 $mv_cmd
