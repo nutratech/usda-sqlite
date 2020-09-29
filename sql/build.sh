11cd5f0dfe97a84148a7cb73904c18443a879612
Shane Jaroch <nutratracker@protonmail.com>
2020-09-29 19:22:58 -0400

    use .sqlite not .db

--- sql/build.sh
+++ sql/build.sh
@@ -10,20 +10,20 @@ fi
 cd "$(dirname "$0")"
 # printf "\\n\e[1;31m\e[0m\\n"
 
-rm_cmd="rm -f usda.db"
+rm_cmd="rm -f usda.sqlite"
 printf "\\n\e[1;31m${rm_cmd}\e[0m\\n\n"
 $rm_cmd
 
-pack_msg="==> Pack usda.db-$VERSION"
+pack_msg="==> Pack usda.sqlite-$VERSION"
 printf "\\n\\x1b[32m${pack_msg}\x1b[0m\n\n"
 
 # Create SQL file
-pack_cmd="sqlite3 usda.db \".read init.sql\""
+pack_cmd="sqlite3 usda.sqlite \".read init.sql\""
 printf "\\n\e[1;31m${pack_cmd}\e[0m\\n"
 bash -exec "$pack_cmd"
 
 # Compress xzip
-tar_cmd="tar cJvf usda.db-$VERSION.tar.xz usda.db"
+tar_cmd="tar cJvf usda.sqlite-$VERSION.tar.xz usda.sqlite"
 printf "\\n\e[1;31m${tar_cmd}\e[0m\\n"
 $tar_cmd
 
@@ -31,6 +31,6 @@ $tar_cmd
 # printf "\\n\e[1;31m${rm_cmd}\e[0m\\n"
 # $rm_cmd
 
-mv_cmd="mv usda.db-$VERSION.tar.xz dist"
+mv_cmd="mv usda.sqlite-$VERSION.tar.xz dist"
 printf "\\n\e[1;31m${mv_cmd}\e[0m\\n\n"
 $mv_cmd
