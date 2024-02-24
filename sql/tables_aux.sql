42c71faa73b89c5bf1acea6626d8df8b92ebd3b7
Shane Jaroch <chown_tee@proton.me>
2024-02-24 11:21:37 -0500

    add std_dev extension for building USDA sqlite

--- sql/tables_aux.sql
+++ sql/tables_aux.sql
@@ -1,3 +1,5 @@
+-- NOTE: need to first run: fetch.sh
+.load ./extensions/stats
 -- Saves time intensive query in new table
 CREATE TABLE nutrients_overview AS
 SELECT
@@ -6,8 +8,9 @@ SELECT
   unit,
   tagname,
   nutr_desc,
-  COUNT(nut_data.nutr_id) AS n_foods,
-  ROUND(avg(nut_data.nutr_val), 3) AS avg_val
+  CAST(COUNT(nut_data.nutr_id) AS INTEGER) AS n_foods,
+  CAST(ROUND(avg(nut_data.nutr_val), 3) AS real) AS avg_val,
+  CAST(ROUND(stddev(nut_data.nutr_val), 2) AS real) AS std_dev
 FROM
   nutr_def
   INNER JOIN nut_data ON nut_data.nutr_id = id
