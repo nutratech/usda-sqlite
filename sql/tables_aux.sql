223a5b958f9adefbd7a5a9efeb96681e50c7729a
Shane Jaroch <chown_tee@proton.me>
2024-02-25 11:33:37 -0500

    update comment/syntax: tables_aux.sql (no changes)

--- sql/tables_aux.sql
+++ sql/tables_aux.sql
@@ -1,6 +1,7 @@
 -- NOTE: need to first run: fetch.sh
 .load ./extensions/stats
 -- Saves time intensive query in new table
+-- TODO: show nutrients_overview.id as a foreign key to nutr_def.id?
 CREATE TABLE nutrients_overview AS
 SELECT
   id,
@@ -8,7 +9,7 @@ SELECT
   unit,
   tagname,
   nutr_desc,
-  CAST(COUNT(nut_data.nutr_id) AS INTEGER) AS n_foods,
+  CAST(COUNT(nut_data.nutr_id) AS integer) AS n_foods,
   CAST(ROUND(avg(nut_data.nutr_val), 3) AS real) AS avg_val,
   CAST(ROUND(stddev(nut_data.nutr_val), 2) AS real) AS std_dev
 FROM
