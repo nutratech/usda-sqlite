33d939cafd687030a6118420c6d72e0bfd9759a4
Shane Jaroch <chown_tee@proton.me>
2024-03-02 14:23:29 -0500

    try-except in latest_version.py

--- sql/latest_version.py
+++ sql/latest_version.py
@@ -11,13 +11,17 @@ import os
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-version_csv_path = os.path.join(
-    SCRIPT_DIR, "version.csv"
-)
+try:
+    version_csv_path = os.path.join(
+        SCRIPT_DIR, "version.csv"
+    )
 
-rows = []
-with open(version_csv_path, "r", encoding="utf-8") as _r_file:
-    reader = csv.reader(_r_file)
-    rows = list(reader)
+    rows = []
+    with open(version_csv_path, "r", encoding="utf-8") as _r_file:
+        reader = csv.reader(_r_file)
+        rows = list(reader)
 
-print(rows[-1][1], end="")
+    print(rows[-1][15], end="")
+except Exception as exc:
+    # Failed, so we return empty version
+    pass
